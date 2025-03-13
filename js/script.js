e94e2c93ff9c4d6962a5b87509a752275fd00716
Gerardo Cornejo <127570809+gerardo-cornejo@users.noreply.github.com>
2025-03-13 00:26:09 -0500

    Certificaciones

--- js/script.js
+++ js/script.js
@@ -13,9 +13,20 @@ function checkearPosicionScroll() {
   lastScrollTop = scrollTop;
 }
 
+$(document).on("click", ".btn-next", () => {
+  //mover el primer elmento de .certificaciones .row .card al final de forma suave
+  let card = $(".certificaciones .row .card").first();
+  $(".certificaciones .row").append(card);
+
+});
+
+$(document).on("click", ".btn-prev", () => {
+  //mover el ultimo elmento de .certificaciones .row .card al principio de forma suave
+  let card = $(".certificaciones .row .card").last();
+  $(".certificaciones .row").prepend(card);
+});
+
 $(document).ready(function () {
-
-
   checkearPosicionScroll();
   window.addEventListener("scroll", checkearPosicionScroll);
 });
